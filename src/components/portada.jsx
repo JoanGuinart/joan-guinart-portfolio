93a300d05d60621e53af48fb9636cf4cc2700354
Joan Guinart <guinartcasasjoan@gmail.com>
2024-10-16 17:33:26 +0200

    fix: portada portfolio bastant animada

--- src/components/portada.jsx
+++ src/components/portada.jsx
@@ -17,10 +17,11 @@ const Portada = () => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: container.current, // L'element que activa l'animació
-        start: "top top", // Inici de l'animació quan la part superior del contenidor coincideix amb la part superior de la finestra
-        end: "+=500", // Final de l'animació després de 500 píxels de desplaçament
+        start: 0, // Inici de l'animació quan la part superior del contenidor coincideix amb la part superior de la finestra
+        isTouch: 2,
+        end: "max", // Final de l'animació després de 500 píxels de desplaçament
         pin: true, // Fixa el contenidor durant l'animació
-        scrub: true, // Fa que l'animació segueixi el desplaçament de la pàgina
+        scrub: 1, // Fa que l'animació segueixi el desplaçament de la pàgina
         markers: true, // Activa marcadors per ajudar a depurar (podeu desactivar-los en producció)
       },
     });
@@ -33,11 +34,12 @@ const Portada = () => {
     )
       .fromTo(
         ".imatge", // Seleccionem l'element .imatge
-        { y: 0, opacity: 1 }, // Estat inicial: no es desplaça i és totalment visible
-        { y: -50, x: 200, opacity: 1, duration: 1 }, // Estat final: es desplaça 50 píxels cap amunt i 200 píxels cap a la dreta, segueix visible
+        { y: 0, filter: "grayscale(1)" }, // Estat inicial: no es desplaça i és totalment visible
+        { y: -50, x: 200, filter: "grayscale(0)", duration: 1 }, // Estat final: es desplaça 50 píxels cap amunt i 200 píxels cap a la dreta, segueix visible
         "<" // Inicia aquesta animació al mateix temps que l'anterior
       )
-      .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 2 }, ">");
+      .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1 }, ">")
+      .fromTo(".text", { y: 0, opacity: 1 }, { y: -200, opacity: 0 }, ">");
   });
 
   // Retornem el JSX del component
@@ -47,12 +49,13 @@ const Portada = () => {
         <div id="nom" className="nom">
           <h1>Joan Guinart</h1>
         </div>
-        <div className="imatge">imatge</div>
+        <div className="imatge"></div>
         <div className="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vel sed
-          laboriosam nisi eveniet saepe assumenda officia facere fugit quisquam
-          asperiores, fugiat aspernatur harum quibusdam voluptatum deleniti
-          ipsum est praesentium!
+          Hello! I'm a FrontEnd developer with a strong track record in
+          effectively tackling complex challenges while pursuing new goals. My
+          technical expertise and soft skills enable me to deliver innovative
+          solutions, collaborate effectively in team settings, and meet tight
+          deadlines, thus contributing to project success. worker.
         </div>
       </div>
       <div id="test" className="test" style={{ height: "1000px" }}></div>{" "}

9026a749f9de5ef80c81fa49bed45935f36a1042
Joan Guinart <guinartcasasjoan@gmail.com>
2024-10-30 18:29:54 +0100

    feat: puntero pro * portada millorada amb ease de gsap

--- src/components/portada.jsx
+++ src/components/portada.jsx
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import "@styles/portada.scss";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -9,12 +9,14 @@ gsap.registerPlugin(ScrollTrigger);
 const Portada = () => {
   const container = useRef();
 
+  const scrollPointer = useRef();
+
   useGSAP(() => {
     const portada = gsap.timeline({
       scrollTrigger: {
         trigger: container.current,
         start: "top top",
-        end: "+=1000",
+        end: "+=4000",
         pin: true,
         scrub: 1,
         markers: true,
@@ -22,27 +24,113 @@ const Portada = () => {
     });
 
     portada
-      .fromTo(".nom", { y: 0 }, { y: -window.innerHeight / 2, duration: 2 })
+      .fromTo(
+        ".nom",
+        { y: 0 },
+        {
+          y: -window.innerHeight / 2,
+          duration: 4,
+          ease: "power1.out",
+        }
+      )
       .fromTo(
         ".text",
         { y: -1 },
-        { y: window.innerHeight / 2, duration: 2 },
+        {
+          y: window.innerHeight / 2,
+          duration: 4,
+          ease: "power1.out",
+        },
+        "<"
+      )
+      .fromTo(
+        ".nom-nom",
+        { y: 0 },
+        { y: -window.innerHeight / 4, duration: 4, ease: "circ.out" },
+        "<"
+      )
+      .fromTo(
+        ".text-text",
+        { y: 0 },
+        { y: window.innerHeight / 4, duration: 4, ease: "circ.out" },
         "<"
       );
+
+    gsap.to(".lorem", {
+      y: window.innerHeight / 6,
+      scrollTrigger: {
+        trigger: ".projectes",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
   });
+
+  useEffect(() => {
+    const containerEl = container.current;
+    const scrollPointerEl = scrollPointer.current;
+
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      scrollPointerEl.style.transform = `translate3d(${clientX - 25}px, ${
+        clientY - 25
+      }px, 0)`;
+    };
+
+    const handleScroll = () => {
+      const scrollY = containerEl.scrollTop;
+      scrollPointerEl.style.transform += ` translateY(${scrollY}px)`;
+    };
+
+    containerEl.addEventListener("mousemove", handleMouseMove);
+    containerEl.addEventListener("scroll", handleScroll);
+
+    return () => {
+      containerEl.removeEventListener("mousemove", handleMouseMove);
+      containerEl.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      <div id="Portada" className="portada" ref={container}>
+      <div id="Portada" className="portada scroll-container" ref={container}>
+        <div className="scroll-pointer" ref={scrollPointer}>SCROLL</div>
         <div id="nom" className="nom">
-          <h1>Joan Guinart</h1>
+          <h1 className="nom-nom">Joan Guinart</h1>
         </div>
         <div className="text">
-          <h2>Frontend Developer</h2>
+          <h2 className="text-text">Frontend Developer</h2>
         </div>
-        <div id="test" className="test"></div>
+        <div id="test" className="test">
+          <p className="lorem">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            asperiores laudantium ab inventore nam neque atque molestias, odit
+            tempore adipisci cumque nemo saepe perferendis enim minus! Minima ab
+            labore non.Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Illum asperiores laudantium ab inventore nam neque atque molestias,
+            odit tempore adipisci cumque nemo saepe perferendis enim minus!
+            Minima ab labore non.Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Illum asperiores laudantium ab inventore nam neque
+            atque molestias, odit tempore adipisci cumque nemo saepe perferendis
+            enim minus! Minima ab labore non.
+          </p>
+        </div>
       </div>
-      <div id="Projectes" className="projectes">miu miau</div>
+      <div
+        id="Projectes"
+        className="projectes"
+        style={{ position: "sticky", top: "0" }}
+      >
+        <p>miu miau</p>
+      </div>
+      <div
+        id="Projectes"
+        className="projectes"
+        style={{ position: "sticky", top: "0", backgroundColor: "#f8f9" }}
+      >
+        <p>miu miau</p>
+      </div>
     </>
   );
 };
